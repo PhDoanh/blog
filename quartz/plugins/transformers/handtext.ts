babb46b83f498ee50727978cf417e24aa6c62213
Pham Doanh <pgdoanh711@gmail.com>
2026-01-26 12:15:42 +0700

    chore: rename poetry to handtext

--- quartz/plugins/transformers/handtext.ts
+++ quartz/plugins/transformers/handtext.ts
@@ -2,15 +2,15 @@ import { QuartzTransformerPlugin } from "../types"
 import { Root } from "mdast"
 import { visit } from "unist-util-visit"
 
-export const Poetry: QuartzTransformerPlugin = () => ({
-	name: "Poetry",
+export const HandText: QuartzTransformerPlugin = () => ({
+	name: "HandText",
 	markdownPlugins() {
 		return [
 			() => (tree: Root, _file) => {
 				visit(tree, "code", (node) => {
-					if (node.lang === "poetry") {
+					if (node.lang === "handtext") {
 						node.type = "html" as "code"
-						node.value = `<pre class="poetry">${node.value}</pre>`
+						node.value = `<pre class="handtext">${node.value}</pre>`
 					}
 				})
 			},
